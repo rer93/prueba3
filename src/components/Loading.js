ea7ebf4004d15f025cf559f25317c237137afcf9
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-10-28 23:20:40 -0500

    primer commit

---
+++ src/components/Loading.js
@@ -0,0 +1,27 @@
+
+
+export default function Loading() {
+    return (
+        <div style={{
+            position: 'fixed',
+            top:'0',
+            left:'0',
+            width:'100vw',
+            height:'100vh',
+            zIndex:99,
+            backgroundColor:'white'
+        }}>
+            <i 
+                className="fas fa-bone fa-spin fa-4x" 
+                style={{
+                    color:'rgb(25, 135, 84)',
+                    position:'absolute',
+                    top:'calc(50% - 50px)',
+                    left:'calc(50% - 50px)'
+                }}
+            />
+
+        </div>
+    )
+}
+
